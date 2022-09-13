3ab97e6d9c2d631f7562c1e203e30040e3f45217
Rastaffo <ramon.carafi@hotmail.com>
2022-09-12 21:31:49 -0300

    segundo

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.listen(3000, () => {
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3030, () => {
     console.log("Servidor funcionando")
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
 })
